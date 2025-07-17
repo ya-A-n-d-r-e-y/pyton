bdf0db40f3367b5b8e1b5082d0b8c3e1ba9b09e6
ya-A-n-d-r-e-y <triabc.online@yandex.ru>
2025-07-17 12:11:56 +0300

    Update python.py

--- python.py
+++ python.py
@@ -1,2 +1,12 @@
-print("Hello World")
-print("Hello World")
+# print("Hello World")
+# print("Hello World")
+var_1 = "Hello World!"
+print(var_1)
+# var_2 = 8//3 Целочисленное деление
+var_2 = str(8) + str(3)
+var_3 = 8//3
+
+
+# print(var_3)
+print(var_2)
+# print(type(var_2))
